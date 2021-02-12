64986aa690b799f5df6d29ef908617ff2dd096aa
Fabio Rosada <r.fabio95@gmail.com>
2021-02-12 12:04:54 +0100

    Used convert_labels.py to export a new format (old code restored, new commented)

--- utils/convert_labels.py
+++ utils/convert_labels.py
@@ -37,12 +37,14 @@ def convert(data_folder):
                     things = label.split(' ')
                     x1, y1, x2, y2, x3, y3, x4, y4 = map(float, things[:8])
                     category = label_to_int[things[8]]
+                    # category = things[8]
                     difficult = things[9]  # not used now
                     xmin = min(x1, x2, x3, x4)
                     xmax = max(x1, x2, x3, x4)
                     ymin = min(y1, y2, y3, y4)
                     ymax = max(y1, y2, y3, y4)
                     print(f'{category} {(xmax-xmin)/2+xmin} {(ymax-ymin)/2+ymin} {xmax-xmin} {ymax-ymin}', file=nf)
+                    # print(f'{category} {xmin} {ymin} {xmax} {ymax}', file=nf)
 
 if __name__ == '__main__':
     convert('data/dota')
